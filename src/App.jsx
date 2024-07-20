bd93e0723b5fff2251a1f7219c2265b05910363a
Marin Milotić <mGitM101@gmail.com>
2024-07-20 11:21:44 +0200

    Replaced the a tag with Link

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,13 @@ import {
 } from "react-router-dom";
 import MainLayout from "./layouts/MainLayout";
 import Homepage from "./pages/Homepage";
+import JobsPage from "./pages/JobsPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<Homepage />} />
+      <Route path="jobs" element={<JobsPage />} />
     </Route>
   )
 );
