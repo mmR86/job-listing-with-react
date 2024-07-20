f116e3a2bc217cace11e506040bbed57b284fff2
Marin Milotić <mGitM101@gmail.com>
2024-07-20 11:48:04 +0200

    404 page

--- src/App.jsx
+++ src/App.jsx
@@ -8,12 +8,14 @@ import {
 import MainLayout from "./layouts/MainLayout";
 import Homepage from "./pages/Homepage";
 import JobsPage from "./pages/JobsPage";
+import NotFoundPage from "./pages/NotFoundPage";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<MainLayout />}>
       <Route index element={<Homepage />} />
       <Route path="jobs" element={<JobsPage />} />
+      <Route path="*" element={<NotFoundPage />} />
     </Route>
   )
 );
